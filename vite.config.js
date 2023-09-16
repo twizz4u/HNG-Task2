683ffccfa63642c4a26071e9ee2cbbab4e4d35c3
twizz4u <tiwatayoaragbaiye@gmail.com>
2023-09-16 16:18:54 +0100

    Deployed website

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/twizz4u.github.io/HNG-Task2/",
+  base: "https://twizz4u.github.io/HNG-Task2/",
 });
